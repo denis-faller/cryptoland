931f05ecf5abb720dee63c3f4163abc9f8927619
Денис Фаллер <faller.d@virage24.ru>
2021-07-20 15:40:25 +0700

    header layout

--- js/script.js
+++ js/script.js
@@ -8,4 +8,15 @@ $(document).ready(function(){
         $(".switch-locale").toggleClass("active");
         $(".locale-list").toggleClass("active");
     });
+    $(".locale-list ul li").click(function(){
+        $(".switch-locale").html($(this).html());
+        $(".switch-locale").toggleClass("active");
+        $(".locale-list").toggleClass("active");
+    });
+    $(".menu-burger-header").click(function(){
+        $(".wrap-menu").toggleClass("active");
+        $(".wrap-burger").toggleClass("active");
+        $(".btn-close").toggleClass("active");
+        $(".wrapper").toggleClass("active");
+    });
 });
