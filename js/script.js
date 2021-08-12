be7ccbf3a39822df8d95712f20e561356724ee01
Денис Фаллер <faller.d@virage24.ru>
2021-08-12 12:43:34 +0700

    header layout

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    if($(window).scrollTop() != 0){
+        $("header").addClass("active");
+     }
     $(".switch-locale").click(function(){
         $(".switch-locale").toggleClass("active");
         $(".locale-list").toggleClass("active");
@@ -17,14 +20,32 @@ $(document).ready(function(){
         $(".wrap-menu").toggleClass("active");
         $(".wrap-burger").toggleClass("active");
         $(".btn-close").toggleClass("active");
+        if($(window).scrollTop() == 0 && $("header").hasClass("active")){
+            $("header").removeClass("active");
+        }
+        else{
+            $("header").addClass("active");
+        }
         $("header").toggleClass("open");
     });
-     $(window).on('scroll', function(){
+    $(window).resize(function() {
+        let curW = $(this).width();
+        if((curW > 1350)){
+            if($(window).scrollTop() == 0){
+                $("header").removeClass("active");
+            }
+            $("header").removeClass("open");
+            $(".wrap-menu").removeClass("active");
+            $(".wrap-burger").removeClass("active");
+            $(".btn-close").removeClass("active");
+        }
+    });
+    $(window).on('scroll', function(){
         if(!$("header").hasClass("active")){
             $("header").addClass("active");
         }
-        else if($(window).scrollTop() == 0){
+        else if(($(window).scrollTop() == 0) && !$("header").hasClass("open")){
             $("header").removeClass("active");
         }
-     });
+    });
 });
